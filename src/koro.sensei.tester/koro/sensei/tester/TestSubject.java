37ef5759b98465e3d2080aaf65991ab8903731fc
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2019-01-10 19:31:25 +0100

    Added Formatted Messages For Assertions
    Assertions can now accept formatting arguments and format the failure
    message before submitting it to the exception.

--- src/koro.sensei.tester/koro/sensei/tester/TestSubject.java
+++ src/koro.sensei.tester/koro/sensei/tester/TestSubject.java
@@ -18,7 +18,8 @@ public interface TestSubject {
 	public void runAllTests() throws TestFailureException;
 	
 	/**
-	 * 
+	 * Assert that the test condition is true, otherwise an exception with the 
+	 * specified failure message is raised.
 	 * 
 	 * @param assertion - a condition, which must be true
 	 * @param failureMessage - the message the thrown exception will carry
@@ -28,6 +29,23 @@ public interface TestSubject {
 	public static void assertTestCondition(boolean assertion, String failureMessage) throws TestFailureException {
 		if (!assertion) {
 			throw new TestFailureException(failureMessage);
+		}
+	}
+	
+	/**
+	 * Assert that the test condition is true, otherwise an exception with the 
+	 * specified failure message is raised.
+	 * The failure message will be formated with the specified arguments.
+	 * 
+	 * @param assertion - a condition, which must be true
+	 * @param formattedFailureMessage - the message the thrown exception will carry
+	 *  if the assertion is false
+	 * @param formatParameters the arguments used to format the failure message
+	 * @throws TestFailureException this will be thrown if the assertion if false
+	 */
+	public static void assertTestCondition(boolean assertion, String formattedFailureMessage, Object... formatParameters) throws TestFailureException {
+		if (!assertion) {
+			throw new TestFailureException(String.format(formattedFailureMessage, formatParameters));
 		}
 	}
 	
